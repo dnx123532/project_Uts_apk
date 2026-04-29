6152bfdb5e924661d7c7feec78453d2b55f39be0
daud <nikolas1133niko@gmail.com>
2026-04-29 22:05:16 +0700

    pengerjaan bagian booking,seat,widget1

--- lib/providers/location_provider.dart
+++ lib/providers/location_provider.dart
@@ -1,11 +1,1 @@
-import 'package:flutter/material.dart';
 
-class LocationState extends ChangeNotifier {
-  String _selectedCity = 'Medan'; // Default awal
-  String get selectedCity => _selectedCity;
-
-  void setCity(String city) {
-    _selectedCity = city;
-    notifyListeners();
-  }
-}
